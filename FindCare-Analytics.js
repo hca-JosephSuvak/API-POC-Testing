ca6301d8abb001685c42e7e3718a28401c16cf5b
Joseph Suvak <robert.suvak@hcahealthcare.com>
2021-06-17 12:45:12 -0500

    Analytics Update
    After meeting with Callaro, we found production endpoints touched to validate the data transmission is operational. So, this takes care of that. The actual click to be handled via Nightwatch.js

--- FindCare-Analytics.js
+++ FindCare-Analytics.js
@@ -1,3 +1,13 @@
+
+var axios = require("axios");
+/* This is the Axios Config where you add required headers. 
+Axios handles the base request automatically with a method and URL.
+External Auth requires extra exception handling.*/
+var config = {
+  method: 'get',
+  url: 'https://medicalcityhealthcare.com/api/content/render/false/type/json/query/+contentType:Feature%20-Feature.cssSelector:na%20-Feature.status:*obsolete*%20+languageId:1%20+deleted:false%20+live:true%20+working:true/orderby/modDate%20desc/limit/400'
+};
+
 async function APIHandlerOnline() {
   /*Axios needs to be Nested within Promise. 
   Notice it has 2 arguments to the Promise: resolve and reject.
@@ -7,9 +17,8 @@ async function APIHandlerOnline() {
       //Then is  a promissory statement. It does this upon initiation.
       .then(function (response) {
         //loggedData variable parses the data into a queriable object.
-        var loggedData = response;
+        var loggedData = response.data;
         //Resolve is the First Callback Function that sends Logged Data
-        console.log(loggedData);
         resolve(loggedData);
       })
       //The catch statement pulls the function.
@@ -22,3 +31,6 @@ async function APIHandlerOnline() {
   })
 
 }
+
+//Must be present to insert the promise into it's called function.
+module.exports = APIHandlerOnline;
---
+++ FindCare-Analytics.spec.js
@@ -0,0 +1,320 @@
+const callAnalyticsMedicalCity = require("./FindCare-Analytics.js");
+
+describe('Data Retrieval - Medical City Analytics [Type]', function () {
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[0].type1));
+        expect(DataExtraction).toBe("syndicated");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[1].type1));
+        expect(DataExtraction).toBe("widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[2].type1));
+        expect(DataExtraction).toBe("app");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[3].type1));
+        expect(DataExtraction).toBe("widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[4].type1));
+        expect(DataExtraction).toBe("widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[5].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[6].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[7].type1));
+        expect(DataExtraction).toBe("widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[8].type1));
+        expect(DataExtraction).toBe("widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[9].type1));
+        expect(DataExtraction).toBe("widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[10].type1));
+        expect(DataExtraction).toBe("widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[11].type1));
+        expect(DataExtraction).toBe("app");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[12].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[13].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[14].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[15].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[16].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[17].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[18].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[19].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[20].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[21].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[22].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[23].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[24].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[25].type1));
+        expect(DataExtraction).toBe("wss");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+}
+);
+describe('Data Retrieval - Medical City Analytics [TechDocs] ', function () {
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[0].type1));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/syndicated-content-privacy-notice");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[1].type1));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/syndicated-content-privacy-notice");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[2].type1));
+        expect(DataExtraction).toBe("n/a");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[3].type1));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/hut-blog-app");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[4].type1));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/formstack-widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[5].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/formstack-widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[6].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/formstack-widget");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[7].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/image-gallery");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[8].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/image-gallery");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[9].techdoc));
+        expect(DataExtraction).toBe("n/a");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[10].techdoc));
+        expect(DataExtraction).toBe("https://web-q-hospital.prod.ehc.com/global/hut/widgets/alert-cards/v1/snippet.dot");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[11].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/our-specialties");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[12].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/our-specialties");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[13].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/marketing-campaign");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[14].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/marketing-campaign");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[15].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/featured-classes-and-events");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[16].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/featured-classes-and-events");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[17].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/facility-spotlight");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[18].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/facility-spotlight");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[19].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/homepage-app");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[20].techdoc));
+        expect(DataExtraction).toBe("https://techdocs.ehc.com/docs/homepage-app");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[21].techdoc));
+        expect(DataExtraction).toBe("https://web-q-hospital.prod.ehc.com/global/hut/widgets/qa-video-gallery/");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[22].techdoc));
+        expect(DataExtraction).toBe("https://web-q-hospital.prod.ehc.com/global/hut/widgets/podcast/");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[23].techdoc));
+        expect(DataExtraction).toBe("https://web-q-hospital.prod.ehc.com/global/hut/widgets/crm-calendar-pull/");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[24].techdoc));
+        expect(DataExtraction).toBe("https://web-q-hospital.prod.ehc.com/global/hut/widgets/timeline-scroller/v1/demo.dot");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+    test('App Type is equal to expected Value', async () => {
+        let data = await callAnalyticsMedicalCity();
+        var DataExtraction = JSON.parse(JSON.stringify(data.contentlets[25].techdoc));
+        expect(DataExtraction).toBe("https://web-q-hospital.prod.ehc.com/global/hut/widgets/stroke-risk-profiler/");
+        //Line 7 verifies the exact match [using toBe command] for this Data Value.
+    })
+}
+);
